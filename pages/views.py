f5d23939aa1f8ef6198cf3594ac6786d43c15669
markbrua <markwilliam.brua@gmail.com>
2025-09-15 19:11:13 +0800

    created .gitignore, project config, pages app, home, base, about templates.

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    context = {
+        'title': 'home',
+        'features': ['Django', 'Templates', 'Static Files'],
+    }
+    return render(request, 'home.html', context)
+
+def about(request):
+
+    return render(request, 'about.html', {'title': 'about'})
